a90d310bc7bc8e681c943ff9bd2d249767c5bfeb
Ian Culp <icculp@gmail.com>
2021-12-28 08:48:23 -0600

    placeholders, stacking

--- src/App.js
+++ src/App.js
@@ -20,21 +20,25 @@ function ContactForm() {
         id="email"
         type="email" 
         name="email"
+        placeholder="Email"
       />
       <ValidationError 
         prefix="Email" 
         field="email"
         errors={state.errors}
       />
+      <br />
       <textarea
         id="message"
         name="message"
+        placeholder="Message"
       />
       <ValidationError 
         prefix="Message" 
         field="message"
         errors={state.errors}
       />
+      <br />
       <button type="submit" disabled={state.submitting}>
         Submit
       </button>
