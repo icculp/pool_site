45724c2dafe9ed7768a5db21313218a5c4c7e8dd
Ian Culp <icculp@gmail.com>
2021-12-28 08:29:16 -0600

    youtube embed

--- src/App.js
+++ src/App.js
@@ -56,6 +56,8 @@ function App() {
 					<br />
 					<iframe title="epoch counter" src="https://stakada.io/embed/epoch" frameborder="0" width="100%" height="60" style={{"max-width":"750px"}} credits="0"></iframe>
 					<br />
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/xX4lf11H5Oo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <br />
 					<a href="https://cardanoscan.io/pool/aa3f700fc539642a938fbcc8b0bb4a37c0fd6b5023fd412f89693aa2?tab=delegators" target="_blank" className="App-link" rel="noopener noreferrer">cardanoscan</a>
 					<br />
 					<a href="https://adatools.io/pools/aa3f700fc539642a938fbcc8b0bb4a37c0fd6b5023fd412f89693aa2" target="_blank" className="App-link" rel="noopener noreferrer">adatools</a>
